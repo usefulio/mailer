7e2f556d8db80d20496f5e5047acfce750e949d8
vladbalan <vlad.balan.dev@gmail.com>
2015-07-02 12:01:04 +0300

    Test cascading defaults

--- tests/tinyTest/mailer-tests.js
+++ tests/tinyTest/mailer-tests.js
@@ -1,5 +1,25 @@
-// Write your tests here!
-// Here is an example.
-Tinytest.add('some example', function (test) {
-  test.equal(true, true, 'some message');
+Tinytest.add('Mailer() - config options - settings json', function (test) {
+    TestMailer = new Mailer();
+
+    test.equal(TestMailer.config.message, 'Settings default', 'The expected value is the default from the settings json file');
+});
+
+Tinytest.add('Mailer() - config options - Mailer() instance', function (test) {
+    TestMailer = new Mailer({
+        message: 'Instance default'
+    });
+
+    test.equal(TestMailer.config.message, 'Instance default', 'The expected value is the one from the Mailer() instance');
+});
+
+Tinytest.add('Mailer() - config options - send() function', function (test) {
+    TestMailer = new Mailer({
+        message: 'Instance default'
+    });
+
+    TestMailer.send("NewMessage", {
+        message: 'Send default'
+    });
+
+    test.equal(TestMailer.config.message, 'Send default', 'The expected value is the one passed to the send() function');
 });
